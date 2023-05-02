1683741ffe64ea8dee831c2f368edc37d7062a59
shunrexd <72516739+shunrexd@users.noreply.github.com>
2023-05-02 18:26:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port 3000");
+});
+
 module.exports = app;
